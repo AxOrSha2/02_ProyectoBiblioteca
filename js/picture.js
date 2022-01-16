3977780dcd6f73d3b7096655d208a694ce6fb1be
AxOrSha2 <90012640+AxOrSha2@users.noreply.github.com>
2022-01-15 23:19:55 -0500

    First commit

---
+++ js/picture.js
@@ -0,0 +1,19 @@
+class picture {
+
+    constructor(
+        pictureSRC,
+        pictureName,
+        pictureDate,
+        pictureMaker,
+        pictureDescription
+    ) {
+        this.pictureSRC = pictureSRC;
+        this.pictureName = pictureName;
+        this.pictureDate = pictureDate;
+        this.pictureMaker = pictureMaker;
+        this.pictureDescription = pictureDescription;
+    }
+
+    
+
+};
