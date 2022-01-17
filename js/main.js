440269bf22c9b19cb063ac73bab9760852ce62f0
AxOrSha2 <90012640+AxOrSha2@users.noreply.github.com>
2022-01-17 00:54:50 -0500

    Chek the requirements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
-let pictureOne = new Picture (
-    'pictureOne', 
+let pictureOne = new Picture(
+    'One',
     'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/a/andy_warhol-5.jpg',
     'Díptico de Marilyn',
     'En el año 1962',
@@ -8,8 +8,9 @@ let pictureOne = new Picture (
     'Tras la muerte de Marilyn Monroe en 1962, Warhol empezó a trabajar en una serigrafía de su rostro, usando una fotografía promocional del filme Niágara (1953). El resultado fueron 50 retratos suyos, la mitad de la izquierda a todo color y los demás en un blanco y negro que se desvanece en lo que representa el culto a la celebridad y la muerte.'
 );
 
-let pictureTwo = new Picture (
-    'pictureTwo', 
+
+let pictureTwo = new Picture(
+    'Two',
     'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/r/roy_lichtenstein_whaam.jpg',
     'Whaam!',
     'En el año 1963',
@@ -17,8 +18,9 @@ let pictureTwo = new Picture (
     'Esta imagen parodia a la vez que documenta la imagen del héroe americano moderno publicada en los cómics de los 70. El uso de viñetas de historietas se convirtió en un sello particular de Lichtenstein y esta es su obra más famosa.'
 );
 
-let pictureThree = new Picture (
-    'pictureThree', 
+
+let pictureThree = new Picture(
+    'Three',
     'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/h/hockney-a_bigger_splash.jpg',
     'El gran chapuzón',
     'El año 1967',
@@ -26,8 +28,9 @@ let pictureThree = new Picture (
     'Sorprendido e inspirado en el estilo de vida californiano, Hockney pintó esta obra en 1967. La imagen es de una casa moderna donde se observa un gran chapuzón de agua creado por una figura no vista que aparentemente saltó desde el trampolín.'
 );
 
-let pictureFour = new Picture (
-    'pictureFour', 
+
+let pictureFour = new Picture(
+    'Four',
     'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/q/que-es-lo-que-hace-a-los-hogares.jpg',
     '¿Qué es lo que hace a los hogares de hoy tan diferentes, tan atractivos?',
     'El año 1967',
@@ -35,14 +38,20 @@ let pictureFour = new Picture (
     'La obra consiste en un collage de imágenes tomadas de revistas americanas para definir la creciente sociedad de consumo a mediados de los 50. El cuadro se convirtió en el ícono del Pop Art y representaba la conciencia posguerra.'
 );
 
-// Here call the function to insert the image in the slider 
-// pictureOne.insertElement();
-// pictureTwo.insertElement();
-// pictureThree.insertElement();
-// pictureFour.insertElement();
 
-// Here call the function to insert the data in the HTML
-pictureOne.setElementsInHTML();
-pictureTwo.setElementsInHTML();
-pictureThree.setElementsInHTML();
-pictureFour.setElementsInHTML();
+let pictureFive = new Picture(
+    'Five',
+    'https://cdn.culturagenial.com/es/imagenes/sopa-campbell-s-cke.jpg',
+    'Latas de sopa Campbell\'s. Serigrafía y polímero sintético sobre tela',
+    'En el año 1962',
+    'Andy Warhol',
+    '¿Quién dijo que una lata de sopa puede ser un tema artístico? Esa fue una de las primeras polémicas que despertó Andy Warhol con esta obra provocadora. Desde la reproducción individual de la sopa hasta la conformación de la serie, Warhol se sitúa irónicamente frente a la realidad de la sociedad de consumo y frente a la comunidad artística que no quiere enfrentarla.',
+);
+
+
+// Here call the function to interact with the pictures
+pictureOne.mouseOverAndOut();
+pictureTwo.appearUsingClick();
+pictureThree.dragAnItem();
+pictureFour.onCopy();
+pictureFive.mouseOverAndOut();
--- js/picture.js
+++ js/picture.js
@@ -23,45 +23,133 @@ class Picture {
     //     return this.pictureName;
     // }
 
-    setElementsInHTML() {
+
+    //Function than find the picture and add the action; mouseover and mouseout 
+    mouseOverAndOut() {
 
         let tittle = this.pictureName;
         let description = this.pictureDescription;
         let maker = this.pictureMaker;
         let date = this.pictureDate;
+        let id = this.pictureID;
 
         document.getElementById(this.pictureID).addEventListener("mouseover", mouseOver);
-
         document.getElementById(this.pictureID).addEventListener("mouseout", mouseOut);
 
         function mouseOver() {
-            document.getElementById("titlePicture").innerHTML = "Título de la obra: " + tittle;
-            document.getElementById("describePicture").innerHTML = "Descripción: " + description;
-            document.getElementById("authorPicture").innerHTML = "Autor: " + maker;
-            document.getElementById("datePicture").innerHTML = "Fecha de creación: " + date;
+            setInfoIntoHMTL(id, tittle, description, maker, date);
         }
 
         function mouseOut() {
-            document.getElementById("titlePicture").innerHTML = "Título de la obra: ";
-            document.getElementById("describePicture").innerHTML = "Descripción: ";
-            document.getElementById("authorPicture").innerHTML = "Autor: ";
-            document.getElementById("datePicture").innerHTML = "Fecha de creación: ";
+            cleanInfoHTML(id);
         }
+    }
+
+
+    //This function made than the info appear if listen a click or disappear when do double click
+    appearUsingClick() {
+
+        let tittle = this.pictureName;
+        let description = this.pictureDescription;
+        let maker = this.pictureMaker;
+        let date = this.pictureDate;
+        let id = this.pictureID;
+
+        document.getElementById(this.pictureID).addEventListener("click", Click);
+        document.getElementById(this.pictureID).addEventListener("dblclick", doubleClick);
+
+        function Click() {
+            setInfoIntoHMTL(id, tittle, description, maker, date);
+        }
+
+        function doubleClick() {
+            cleanInfoHTML(id);
+        }
+    }
+
+
+    //This function made than the info appear if the picture are draged or disappear when do double click
+    dragAnItem() {
+
+        let tittle = this.pictureName;
+        let description = this.pictureDescription;
+        let maker = this.pictureMaker;
+        let date = this.pictureDate;
+        let id = this.pictureID;
+
+        document.getElementById(this.pictureID).addEventListener("drag", Drag);
+        document.getElementById(this.pictureID).addEventListener("dblclick", doubleClick);
+
+        function Drag() {
+            setInfoIntoHMTL(id, tittle, description, maker, date);
+        }
+
+        function doubleClick() {
+            cleanInfoHTML(id);
+        }
+    }
+
+
+    //This function made than the info appear if the picture are copied or disappear when do double click
+    onCopy() {
+
+        let tittle = this.pictureName;
+        let description = this.pictureDescription;
+        let maker = this.pictureMaker;
+        let date = this.pictureDate;
+        let id = this.pictureID;
+
+        document.getElementById("instructions-four").addEventListener("copy", Copy);
+        document.getElementById(this.pictureID).addEventListener("drag", Drag);
+
+        function Copy() {
+            setInfoIntoHMTL(id, tittle, description, maker, date);
+        }
+
+        function Drag() {
+            cleanInfoHTML(id, tittle, description, maker, date);
+        }
+    }
+
+    //TODO
+    setPictureSkeletonIntoHTML() {
+
+        //Get the template and made a copy 
+        let template = document.querySelector('#card-template').cloneNode(true);
+        //Change attributes of the copy
+        template.setAttribute('#card-template', 'card');
+
+
+        //Add the template into card-container
+        let sectionContainer = document.getElementById("card-container");
+        sectionContainer.appendChild(template);
 
     }
 
     prueba() {
-        console.log("Prueba de: " + this.pictureID
-            + " de nombre: " + this.pictureName);
+        console.log("\n Prueba de:  " + this.pictureID
+            + "\n src:      " + this.pictureSRC
+            + "\n Nombre:   " + this.pictureName
+            + "\n Fecha:    " + this.pictureDate
+            + "\n Creador:  " + this.pictureMaker
+            + "\n Descrip:  " + this.pictureDescription
+        );
     }
-
-    // TODO 
-    insertElement(){
-        let id = this.pictureID;
-        let src = this.pictureSRC;
-
-        document.getElementById("slider-images").innerHTML = '<li><img id="'+id+'" src="'+src+'" alt=""></li>';
-
-    }
-
 };
+
+//Set infomation into HTML
+function setInfoIntoHMTL(id, tittle, description, maker, date) {
+    document.getElementById("titlePicture" + id).innerHTML = "Título de la obra: " + tittle;
+    document.getElementById("describePicture" + id).innerHTML = "Descripción: " + description;
+    document.getElementById("authorPicture" + id).innerHTML = "Autor: " + maker;
+    document.getElementById("datePicture" + id).innerHTML = "Fecha de creación: " + date;
+}
+
+
+// Clean the camps 
+function cleanInfoHTML(id) {
+    document.getElementById("titlePicture" + id).innerHTML = "Título de la obra: ";
+    document.getElementById("describePicture" + id).innerHTML = "Descripción: ";
+    document.getElementById("authorPicture" + id).innerHTML = "Autor: ";
+    document.getElementById("datePicture" + id).innerHTML = "Fecha de creación: ";
+}
